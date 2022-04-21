ff03e235574323c42c8e1a9579da5828de2f3155
Alfredo Pereda <aperar1500@alumno.ipn.mx>
2022-04-21 14:36:17 -0500

    Implementation the Spiderman Test applying TDD

--- test/spiderman.test.js
+++ test/spiderman.test.js
@@ -1,7 +1,23 @@
 
 describe("Test Suite Description", () => {
-    Test("Case 1 Dummy", () => {
+    test("Case 1 Dummy", () => {
         const resultOfSomething = 1 + 2
-        expect(resultOfSomething).toBe(5)
+        expect(resultOfSomething).toBe(3)
     });
+})
+
+describe("Unit Test for Spiderman Class", () => {
+    test('1 ) Create a Spiderman Object', () => {
+        
+        //Aqui escribimos el código que queremos usar tal cuál
+        //Quierp poder instanciar un objeto Spiderman con está información
+        const andrewGarfield = new Spiderman("Spiderman Sony", 31, "Andrew Garfield", 2, "Sony Picture")
+    
+        //Validamos que este código funcione de la forma que esperamos
+        expect(andrewGarfield.name).toBe("Spiderman Sony")
+        expect(andrewGarfield.age).toBe(31)
+        expect(andrewGarfield.actor).toBe("Andrew Garfield")
+        expect(andrewGarfield.movies).toBe(2)
+        expect(andrewGarfield.studio).toBe("Sony Picture")  
+    })
 })
